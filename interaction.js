af42a7ff53ffb550cafe022bd737fb31c139a306
zozomoloko <77685550+zzmlk@users.noreply.github.com>
2021-02-25 08:34:13 -0500

    second click jQuery

--- interaction.js
+++ interaction.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
+    let clickCount = 0;
 
     $('.p1').click(function(e) {
-        $('.title').toggleClass('fullscreen');
+        if (clickCount % 2 == 0) {
+            $('.title').toggleClass('fullscreen');
+        } else {
+            $(".title, .p1").fadeOut(1000);
+        }
+        clickCount++;
     });
 
-    $(".p1").click(function(e) {
-        $(".title, .p1").fadeOut(1000);
-
+    $('.p2').click(function(e) {
+        if (clickCount % 2 == 0) {
+            $('.author').toggleClass('fullscreen');
+        } else {
+            $(".author, .p2").fadeOut(1000);
+        }
+        clickCount++;
     });
-
 });
--- test.js
+++ test.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
+    let clickCount = 0;
 
     $('.p1').click(function(e) {
-        $('.title').toggleClass('fullscreen');
+        if (clickCount % 2 == 0) {
+            $('.title').toggleClass('fullscreen');
+        } else {
+            $(".title, .p1").fadeOut(1000);
+        }
+        clickCount++;
     });
 
-    $(".p1").click(function(e) {
-        $(".title, .p1").fadeOut(1000);
-
+    $('.p2').click(function(e) {
+        if (clickCount % 2 == 0) {
+            $('.author').toggleClass('fullscreen');
+        } else {
+            $(".author, .p2").fadeOut(1000);
+        }
+        clickCount++;
     });
-
 });
+
+// $(document).ready(function() {
+//     var toggle = false;
+//     $('.p1').click(function(e) {
+//         if (toggle === false) {
+//             $('.title').toggleClass('fullscreen');
+//             toggle = true;
+//         } else {
+//             $(".title, .p1").fadeOut(1000);
+//         }
+//     });
+// });
+
+// $(document).ready(function() {
+//     var toggle = false;
+//     $('.p2').click(function(e) {
+//         if (toggle === false) {
+//             $('.author').toggleClass('fullscreen');
+//             toggle = true;
+//         } else {
+//             $(".author, .p2").fadeOut(1000);
+//         }
+//     });
+// });
