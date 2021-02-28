a7c494a2b43a54a5836e7fae6d24aaa9c08ad323
zozomoloko <77685550+zzmlk@users.noreply.github.com>
2021-02-28 00:00:33 -0500

    finish_1version

--- interaction.js
+++ interaction.js
@@ -1,55 +1,12 @@
-// $(document).ready(function() {
-//     let clickCount = 0;
-
-//     $('#move').click(function(e) {
-//         if (clickCount % 2 == 0) {
-//             $('.title').toggleClass('fullscreen');
-//         } else {
-//             $(".p1, .title").fadeOut(1000);
-//         }
-//         clickCount++;
-//         console.log(clickCount);
-//     });
-
-
-// $(document).ready(function() {
-
-
-//     $('#move').click(function(e) {
-
-//         $('.title').toggleClass('fullscreen').addClass('max');
-
-//         // if ($(".title").hasClass('max')) {
-
-//         $('.p1, .title').delay(2000).fadeOut(1000);
-
-//         $('.author').toggleClass('fullscreen').addClass('max');
-//         $('.p2, .author').delay(2000).fadeOut(1000);
-
-
-//         // }
-
-
-//     });
-
-
-// });
-
 $(document).ready(function() {
     let clickCount = 0;
     $('.word').click(function(e) {
         const attr = e.target.getAttribute('clickedonce');
-        const closestNode = e.target.closest('.layer');
+        const closestNode = e.target.closest('#layer');
         if (attr) {
             e.target.setAttribute('clickedonce', false);
             $(closestNode).find('.blur').fadeOut(1000);
             $(closestNode).find('.word').fadeOut(1000);
-            // $(closestNode).find('.blur').css('z-index', -10);
-            // $(closestNode).find('.word').css('z-index', -10);
-
-
-
-
         } else {
             $(closestNode).find('.blur').toggleClass('fullscreen');
             // Set a custom attribute to the parent node
@@ -57,19 +14,9 @@ $(document).ready(function() {
         }
     });
 
-    // $('.p2').click(function(e) {
-    //     const attr = e.target.getAttribute('clickedonce');
-    //     const closestNode = e.target.closest('.container');
-    //     if (attr) {
-    //         e.target.setAttribute('clickedonce', false);
-    //         $(closestNode).find('.author').fadeOut(1000);
-    //         $(closestNode).find('.p2').fadeOut(1000);
 
+    $(".button").click(function() {
+        location.reload(true);
+    });
 
-    //     } else {
-    //         $(closestNode).find('.author').toggleClass('fullscreen');
-    //         // Set a custom attribute to the parent node
-    //         e.target.setAttribute('clickedonce', true);
-    //     }
-    // });
 });
